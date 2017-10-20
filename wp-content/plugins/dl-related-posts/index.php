e5147f05efe028abc64ea8c63c1cd80f2847ed6c
Dmytrii Lysak <dlysak@live.com>
2017-10-20 13:25:36 +0300

    6+

---
+++ wp-content/plugins/dl-related-posts/index.php
@@ -0,0 +1,11 @@
+<?php /**
+ * Plugin Name: Похожие записи
+ * Description: Плагин выводит ссылки на похожие записи.
+ * Plugin URI: http://lysak.github.io
+ * Author: Dmytrii Lysak
+ * Author URI: http://lysak.github.io
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: text-domain
+ * Domain Path: domain/path
+ */
