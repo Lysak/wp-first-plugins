10d5ceae0fee65f10f3254b76062c0eeb772a90f
Dmytrii Lysak <dlysak@live.com>
2017-10-23 00:25:31 +0300

    8+

--- wp-content/plugins/dl-related-posts/dl-related-posts.php
+++ wp-content/plugins/dl-related-posts/dl-related-posts.php
@@ -36,7 +36,14 @@ function dl_related_posts($content) {
 
         while ($related_posts->have_posts()) {
             $related_posts->the_post();
-            $content .= '<a href="' . get_permalink() . '">' . get_the_title() . '</a></br>';
+
+            if(has_post_thumbnail()) {
+                $img = get_the_post_thumbnail(get_the_ID(), array(100, 100), array('alt' => get_the_title(), 'title' => get_the_title()));
+            }else{
+                $img = '<img src="' . plugins_url('images/no_img.jpg', __FILE__) . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" width="100" height="100">';
+            }
+
+            $content .= '<a href="' . get_permalink() . '">' . $img . '</a>';
         }
 
         $content .= '</div>';
