8eac875600873b5a2849e1f85383966fcb95ee98
Dmytrii Lysak <dlysak@live.com>
2017-10-24 20:14:22 +0300

    12+

--- wp-content/plugins/dl-count-views/uninstall.php
+++ wp-content/plugins/dl-count-views/uninstall.php
@@ -2,7 +2,13 @@
 
 if( !defined('WP_UNINSTALL_PLUGIN')) exit;
 
+include(__FILE__, 'dl_create_field');
+
 global $wpdb;
 
-$query = "ALTER TABLE $wpdb->posts DROP dl_views";
-$wpdb -> query($query);
+if(!dl_check_field('dl_views')){
+    $query = "ALTER TABLE $wpdb->posts DROP dl_views";
+    $wpdb -> query($query);
+}
+
+
