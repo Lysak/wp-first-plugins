6dd04eed7473e4578b8762f88243ba6388b46c23
Dmytrii Lysak <dlysak@live.com>
2017-10-26 14:21:34 +0300

    14+

--- wp-content/plugins/dl-auth-captcha/dl-auth-captcha.php
+++ wp-content/plugins/dl-auth-captcha/dl-auth-captcha.php
@@ -1,11 +1,33 @@
 <?php /**
  * Plugin Name: Простая капча для формы авторизации
- * Description: Плагин удаляет поле для сайта и добавляет чекбокс для проверки на человечность.
+ * Description: Плагин добавляет простую проверку на человечность к форме авторизации.
  * Plugin URI: http://lysak.github.io
  * Author: Dmytrii Lysak
  * Author URI: http://lysak.github.io
  * Version: 1.0.0
- * License: GPL2
- * Text Domain: text-domain
- * Domain Path: domain/path
  */
+
+//  add_filter('login_errors', 'my_login_errors');
+
+//  function my_login_errors(){
+//      return 'Ошибка авторизации';
+//  }
+
+// add_action('login_form', 'dl_captcha_login');
+// add_action('wp_authenticate', 'dl_authenticate', 10, 2);
+
+// function dl_captcha_login(){
+//     echo '<p><label class="login-checked" style="font-size: 12px;" for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
+// }
+
+// function dl_authenticate(){
+//     if(isset($_POST['check']) && $_POST['check'] == 'check'){
+//         // wp_die( '<b>Ошибка</b>: вы не прошли проверку на человечность');
+//         add_filter('login_errors', 'my_login_errors');
+//         $username = null;
+//     }
+// }
+
+// function my_login_errors(){
+//     return '<b>Ошибка</b>: вы не прошли проверку на человечность';
+// }
