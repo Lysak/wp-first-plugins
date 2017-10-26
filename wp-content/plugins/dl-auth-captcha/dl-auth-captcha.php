ebcc72f99ee0779f0235d7a0ae925ebbdb3c02bf
Dmytrii Lysak <dlysak@live.com>
2017-10-26 09:35:18 +0300

    14..

---
+++ wp-content/plugins/dl-auth-captcha/dl-auth-captcha.php
@@ -0,0 +1,11 @@
+<?php /**
+ * Plugin Name: Простая капча для формы авторизации
+ * Description: Плагин удаляет поле для сайта и добавляет чекбокс для проверки на человечность.
+ * Plugin URI: http://lysak.github.io
+ * Author: Dmytrii Lysak
+ * Author URI: http://lysak.github.io
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: text-domain
+ * Domain Path: domain/path
+ */
