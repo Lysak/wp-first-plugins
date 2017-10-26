8695a9242d9d4f7efbb51823ef38d52f2f828740
Dmytrii Lysak <dlysak@live.com>
2017-10-26 15:40:02 +0300

    15+

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # wp-firstplugin
 
 git rm -r --cached /var/www/wordpress.loc - удалить файлы из репозитория (переиндексировать)
+
+shift+alt+a - multiline comment vscode
+shift+alt+up/down - дублировать строку 
+
+<!-- 
+alias ll='ls -l'
+alias ls='ls -F --color=auto --show-control-chars'
+-->
+
+alias st='git status'
+alias add='git add .'
+alias cm='git commit -m '
+alias push='git push -u origin master'
+alias pull='git pull'
--- wp-content/plugins/dl-auth-captcha/dl-auth-captcha.php
+++ wp-content/plugins/dl-auth-captcha/dl-auth-captcha.php
@@ -7,27 +7,46 @@
  * Version: 1.0.0
  */
 
-//  add_filter('login_errors', 'my_login_errors');
+ /*add_filter('login_errors', 'my_login_errors');
 
-//  function my_login_errors(){
-//      return 'Ошибка авторизации';
-//  }
+ function my_login_errors(){
+     return 'Ошибка авторизации';
+ }*/
 
-// add_action('login_form', 'dl_captcha_login');
-// add_action('wp_authenticate', 'dl_authenticate', 10, 2);
+/* add_action('login_form', 'dl_captcha_login');
+add_action('wp_authenticate', 'dl_authenticate', 10, 2);
 
-// function dl_captcha_login(){
-//     echo '<p><label class="login-checked" style="font-size: 12px;" for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
-// }
+function dl_captcha_login(){
+    echo '<p><label class="login-checked" style="font-size: 12px;" for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
+}
 
-// function dl_authenticate(){
-//     if(isset($_POST['check']) && $_POST['check'] == 'check'){
-//         // wp_die( '<b>Ошибка</b>: вы не прошли проверку на человечность');
-//         add_filter('login_errors', 'my_login_errors');
-//         $username = null;
-//     }
-// }
+function dl_authenticate(){
+    if(isset($_POST['check']) && $_POST['check'] == 'check'){
+        // wp_die( '<b>Ошибка</b>: вы не прошли проверку на человечность');
+        add_filter('login_errors', 'my_login_errors');
+        $username = null;
+    }
+}
 
-// function my_login_errors(){
-//     return '<b>Ошибка</b>: вы не прошли проверку на человечность';
-// }
+function my_login_errors(){
+    return '<b>Ошибка</b>: вы не прошли проверку на человечность';
+} */
+
+add_action('login_form', 'dl_captcha_login');
+add_action('authenticate', 'dl_auth_signon', 30, 3);
+
+function dl_auth_signon($user, $username, $password){
+    if(isset($_POST['check']) && $_POST['check'] == 'check'){
+        $user = new WP_Error('broke', '<b>Ошибка авторизации</b>: Вы не прошли проверку на человечность');
+        
+    }
+
+    if(isset($user->errors['invalid_username']) || isset($user->errors['incorrect_password'])){
+        return new WP_Error('broke', '<b>Ошибка авторизации</b>: Вы ввели неверный логин или пароль');
+    }
+    return $user;
+}
+
+function dl_captcha_login(){
+    echo '<p><label class="login-checked" style="font-size: 12px;" for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
+}
