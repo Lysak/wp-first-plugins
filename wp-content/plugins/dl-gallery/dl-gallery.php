774556c15636cabd50de3812efd97ed4a398b4f0
Dmytrii Lysak <dlysak@live.com>
2017-11-28 22:40:14 +0200

    22+

--- wp-content/plugins/dl-gallery/dl-gallery.php
+++ wp-content/plugins/dl-gallery/dl-gallery.php
@@ -23,8 +23,13 @@ function dl_gallery( $atts ) {
         $img_desc    = $img_data[0]->post_content;
         $img_caption = $img_data[0]->post_excerpt;
         $img_title   = $img_data[0]->post_title;
-        echo $img_desc . '|' . $img_caption . '|' . $img_title . '<br />';
+        // echo $img_desc . '|' . $img_caption . '|' . $img_title . '<br />';
+        $img_thumb = wp_get_attachment_image_src( $item );
+        var_dump($img_full);
+        $img_full = wp_get_attachment_image_src( $item, 'full' );
+        $html .= "<a href='{$img_full[0]}' data-lightbox='gallery' data-title='{$img_caption}'><img src='{$img_thumb[0]}' width='{$img_thumb[1]}' height='{$img_thumb[2]}' alt='{$img_title}'></a>";
     }
     $html .= '</div>';
+
     return $html;
 }
